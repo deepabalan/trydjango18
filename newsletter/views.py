12989cdb614e1cc8af6b2c6a6c66136f362b3f93
deepabalan <deepa.deepabalan@gmail.com>
2017-12-25 23:29:20 +0530

    added custom form in a view.

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,11 +1,46 @@
 from django.shortcuts import render
 
 
+from .forms import ContactForm, SignUpForm
 def home(request):
     title = 'Welcome'
-    if request.user.is_authenticated():
-        title = "My Title %s" % (request.user)
+    form = SignUpForm(request.POST or None)
     context = {
-        "template_title": title,
+        "title": title,
+        "form": form
     }
+
+    if form.is_valid():
+#        form.save()
+        # print request.POST['email'] # not recommended
+        instance = form.save(commit=False)
+
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+#        if not instance.full_name:
+#            instance.full_name = "Deepa"
+        instance.save()
+#        print instance.email
+#        print instance.timestamp
+        context = {
+            "title": "Thank You"
+        }
     return render(request, "home.html", context)
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        for key, value in form.cleaned_data.iteritems():
+            print key, value
+           # print form.cleaned_data.get(key)
+        #email = form.cleaned_data.get("email")
+        #message = form.cleaned_data.get("message")
+        #full_name = form.cleaned_data.get("full_name")
+        #print email, message, full_name
+    context = {
+        "form": form,
+    }
+    return render(request, "forms.html", context)
